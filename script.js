509ab40095e7425cf432b14eb678dad658875800
sehrishbashir <49878787+sehrishbashir@users.noreply.github.com>
2019-07-16 18:01:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// function addTwoNumbers(numb1, numb2) {
+//     return numb1 + numb2;
+// }
+// console.log(addTwoNumbers(3, 6));
+
+
+
+// function multipleSquare(numb) {
+//     return numb * numb;
+// }
+// console.log(multipleSquare(2));
+
+
+
+// function multipleCube(numb) {
+//     return numb * numb * numb;
+// }
+// console.log(multipleCube(4));
+
+
+
+// function greeting(name) {
+//     return "Eid Mubarak " + name;
+// }
+// console.log(greeting("Armish"));
+
+
+
+// let greet = function(name) {
+//     return "Eid Mubarak " + name;
+// }
+// console.log(greet("Malahil"));
+
+
+
+// let myCar = {
+//     name: "WB",
+//     engineNo: 2324,
+//     modelNo: 2173,
+//     color: "red",
+//     isNew: false,
+//     seatCount: 5,
+//     tyreCount: 4,
+//     hasAC: true,
+//     milage: 437,
+//     fuelTypes: ["petrol", "CNG"],
+//     accelerate: function() {
+//         console.log("ACcelerating!!!");
+//     },
+//     applyBrakes: function() {
+//         console.log("STOP!");
+//     }
+// }
+
+// console.log(myCar);
+// myCar["accelerate"]()
